8991fec2a969623931233aea0561673690828919
Rayya <rayyatar@gmail.com>
2022-04-08 08:28:10 +0200

    Add toggle feature

--- src/screens/ShoppingScreen.jsx
+++ src/screens/ShoppingScreen.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 import TaskList from "../components/TaskList";
 import Sorter from "../components/Sorter"
 
 export default function ShoppingScreen({ listState, openModal }) {
     const [list, setList] = listState;
+
+    //Local state
+    const [showCompleted, setShowCompleted] = useState(false);
+
+    // Properties
+    const completedItems = list.filter((item) => item.completed === true);
+    const pendingItems = list.filter((item) => item.completed === false);
+    const toggleLabel = showCompleted ? "Hide" : "View";
 
     // Methods
   function onCheck(id) {
@@ -14,17 +24,16 @@ export default function ShoppingScreen({ listState, openModal }) {
     setList(clonedList);
   }
 
-  // // Component
-  // const TasksItems = list.map((item) => (
-  //   <TaskItem key={item.id} item={item} onCheck={onCheck} />
-  // ));
-
   return (
     <div>
         <h1>Shopping List</h1>
         <Sorter listState={[list,setList]} />
-        <TaskList list={list} onCheck={onCheck} />
+        <TaskList list={pendingItems} onCheck={onCheck} />
       <button onClick={openModal}>Add item</button>
+      <button onClick={() => setShowCompleted(!showCompleted)}>
+        {toggleLabel} completed items
+      </button>
+      {showCompleted && <TaskList list={completedItems} onCheck={onCheck} />}
     </div>
   )
 }
